8a1a8ad5151f1cf957635fde4603645e148ad3c9
alvarikola <alvaroe484@gmail.com>
2024-03-19 13:03:50 +0100

    intento de terminar numerosRomanos.py

--- numerosRomanos.py
+++ numerosRomanos.py
@@ -1,10 +1,11 @@
 class NumerosRomanos:
-    def __init__(self, numero:int) -> None:
+    def __init__(self, numero:int, romano:str) -> None:
         self.numero = numero
+        self.romano = romano
 
 
 
-    def convertir(self):
+    def convertirDecimales(self):
         listaRomanos = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
         listaNumeros = [1000, 900, 500, 400,  100,  90,  50,  40,    10,  9,    5,   4,     1]
         numeroRomano = ''
@@ -20,13 +21,32 @@ class NumerosRomanos:
             else:
                 i += 1
         return numeroRomano
+    
+
+    def convertirRomanos(self):
+        listaRomanos = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        listaNumeros = [1000, 900, 500, 400,  100,  90,  50,  40,    10,  9,    5,   4,     1]
+        numeroDecimal = 0
+        i = ""
+        while self.romano:
+            if self.romano == listaRomanos[0]:
+                self.romano = listaNumeros[0]
+                return self.romano
+            
+            if self.romano - listaRomanos[i] >= 0:
+                numeroDecimal += listaNumeros[i]
+                self.romano -= listaRomanos[i]
+            else:
+                i += 1
+        return numeroDecimal
 
 
 
 def main():
-    numero1 = NumerosRomanos(134)
+    numero1 = NumerosRomanos(134, "I")
 
-    print(numero1.convertir())
+    print(numero1.convertirDecimales())
+    print(numero1.convertirRomanos())
 
 
 main()
